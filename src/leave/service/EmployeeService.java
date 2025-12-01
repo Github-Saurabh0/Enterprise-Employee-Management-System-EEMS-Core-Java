cc91c79102c55fa63e251b9fa00b0ec1fd2edbe9
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-12-01 11:25:02 +0530

    Added sorting methods (name, salary, joining date, department, role) in EmployeeService

--- src/leave/service/EmployeeService.java
+++ src/leave/service/EmployeeService.java
@@ -2,6 +2,7 @@ package service;
 
 import dao.EmployeeDao;
 import model.Employee;
+import utils.EmployeeComparators;
 
 import java.util.List;
 
@@ -18,18 +19,37 @@ public class EmployeeService {
     }
 
     public List<Employee> getAllEmployees() {
-        return dao.findAll();
+        return dao.getAll();
     }
 
-    public Employee getEmployeeById(int id) {
-        return dao.findById(id);
+    // NEW: Sorting utilities
+    public List<Employee> sortByName() {
+        List<Employee> list = dao.getAll();
+        list.sort(EmployeeComparators.SORT_BY_NAME);
+        return list;
     }
 
-    public boolean updateEmployee(Employee e) {
-        return dao.update(e);
+    public List<Employee> sortBySalary() {
+        List<Employee> list = dao.getAll();
+        list.sort(EmployeeComparators.SORT_BY_SALARY);
+        return list;
     }
 
-    public boolean deleteEmployee(int id) {
-        return dao.delete(id);
+    public List<Employee> sortByJoiningDate() {
+        List<Employee> list = dao.getAll();
+        list.sort(EmployeeComparators.SORT_BY_JOINING_DATE);
+        return list;
+    }
+
+    public List<Employee> sortByDepartment() {
+        List<Employee> list = dao.getAll();
+        list.sort(EmployeeComparators.SORT_BY_DEPARTMENT);
+        return list;
+    }
+
+    public List<Employee> sortByRole() {
+        List<Employee> list = dao.getAll();
+        list.sort(EmployeeComparators.SORT_BY_ROLE);
+        return list;
     }
 }
