0ad73f6fa33a8863efbda8bef7df65adfd09604e
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-12-02 13:24:04 +0530

    Improved FileEmployeeDaoImpl error handling with descriptive log messages

--- src/payroll/dao/FilePayrollDaoImpl.java
+++ src/payroll/dao/FilePayrollDaoImpl.java
@@ -1,31 +1,14 @@
-package payroll.dao;
+@Override
+public boolean save(Employee e) {
+    try (FileWriter fw = new FileWriter(filePath, true);
+         BufferedWriter bw = new BufferedWriter(fw)) {
 
-import payroll.model.Payroll;
+        bw.write(e.toString());
+        bw.newLine();
+        return true;
 
-import java.util.List;
-
-public class FilePayrollDaoImpl implements PayrollDao {
-
-    private final String filePath;
-
-    public FilePayrollDaoImpl(String filePath) {
-        this.filePath = filePath;
-    }
-
-    @Override
-    public void save(Payroll payroll) {
-        // TODO: Write payroll data to CSV
-    }
-
-    @Override
-    public List<Payroll> findAll() {
-        // TODO: Read CSV and return objects
-        return List.of();
-    }
-
-    @Override
-    public Payroll findByEmployeeId(int employeeId) {
-        // TODO: Search by employee ID
-        return null;
+    } catch (IOException ex) {
+        System.err.println("ERROR: Could not save employee → " + ex.getMessage());
+        return false;
     }
 }
